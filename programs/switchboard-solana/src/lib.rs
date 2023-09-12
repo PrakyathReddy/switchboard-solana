094bcfda080c03fa9a6f904cb51e2382a9424266
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-12 18:03:45 +0530

    pulling data from oracle

--- programs/switchboard-solana/src/lib.rs
+++ programs/switchboard-solana/src/lib.rs
@@ -1,21 +1,3 @@
-// use anchor_lang::prelude::*;
-
-// declare_id!("GaUggidmxsP3ZvfzbQ6AcxYmWimZerLqeoAGkHjhsiEk");
-
-// #[program]
-// pub mod switchboard_solana {
-//     use super::*;
-
-//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-//         Ok(())
-//     }
-// }
-
-// #[derive(Accounts)]
-// pub struct Initialize {}
-
-// --------------
-
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::clock;
 use switchboard_solana::AggregatorAccountData;
